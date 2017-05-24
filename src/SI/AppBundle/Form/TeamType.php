d0eae60e444c15defa4160576864c0d64d097105
tderot <derot7@gmail.com>
2017-05-24 17:39:00 +0200

    creation du CRUD

---
+++ src/SI/AppBundle/Form/TeamType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SI\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('text')->add('linkedin')->add('picture');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SI\AppBundle\Entity\Team'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'si_appbundle_team';
+    }
+
+
+}
