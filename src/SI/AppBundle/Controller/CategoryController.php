75629b239884f161402843171b2bbb116824b671
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-05-24 16:19:28 +0200

    Materialize

---
+++ src/SI/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SI\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class CategoryController extends Controller
+{
+    /**
+     * @Route(
+     *     "/category/{id}",
+     *     name="si_app_category",
+     *     defaults={"id": 1},
+     *     requirements={
+     *         "id": "\d+"
+     *     }
+     * )
+     */
+    public function categoryAction()
+    {
+        return $this->render('SIAppBundle:Category:category.html.twig');
+
+    }
+}
