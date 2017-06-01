69c5a41d017e754e7ec87b00048c550e190289d4
Frankois21 <francois.ponceau@gmail.com>
2017-06-01 17:49:35 +0200

    CGV et Mentions en modales

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,11 @@
+// script pour le slider video
+
 $(document).ready(function(){
     $('.slider').slider();
+});
+// script pour la modale des CGV
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
 });
 
