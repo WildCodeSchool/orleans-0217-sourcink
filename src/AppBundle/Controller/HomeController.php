088e4daebc8c08d4697e50deab1df67eaa394abd
Frankois21 <francois.ponceau@gmail.com>
2017-06-09 13:49:23 +0200

    affichage des offres front et offres

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -10,8 +10,25 @@ class HomeController extends Controller
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homeAction()
+    public function homeAction(Api $service)
     {
-        return $this->render('AppBundle:Home:home.html.twig');
+        $data = $service->api('jobs', ["field: duration", "filter: contains", "value: rejected", "custom_fields"]);
+        $i = 1;
+        foreach ($data->_embedded->jobs as $job) {
+            $offers[$job->id] = [
+                'title' => $job->title,
+                'duration' => $job->duration,
+                'description' => $job->description,
+                'city' => $job->location->city,
+                //'name' => $job->_embedded->custom_fields->_embedded->definition->name
+            ];
+            if($i==3){
+                break;
+            }
+            $i++;
+        }
+        //dump($data);
+        //die();
+        return $this->render('AppBundle:Home:home.html.twig', ['offers' => $offers]);
     }
 }
