58159f5d9302f60bf0ed7e94dff729067bf44493
tderot <derot7@gmail.com>
2017-06-20 11:50:33 +0200

    mise en place des filtres 2/3

--- src/AppBundle/Services/Api.php
+++ src/AppBundle/Services/Api.php
@@ -20,17 +20,17 @@ class Api
      * @param array $params
      * @return mixed
      */
-    public function api($query, $params = [], $page=1, $parPage=9)
+    public function api($query, $params = [], $page = 1, $parPage = 100)
     {
         $filters = '';
 
-        foreach($params as $param=>$value){
-            $filters .= $param.':'.$value;
+        foreach ($params as $param => $value) {
+            $filters .= $param . ':' . $value;
         }
-        $url = 'https://api.catsone.com/v3/'.$query.'?per_page='.$parPage.'&page='.$page;
+        $url = 'https://api.catsone.com/v3/' . $query . '?per_page=' . $parPage . '&page=' . $page;
 
         $apiKey = '52190b469513a91f73c29789304acd48';
-        $headers = array('Authorization: Token '.$apiKey, $filters);
+        $headers = array('Authorization: Token ' . $apiKey, $filters);
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -39,4 +39,29 @@ class Api
         return json_decode($response);
     }
 
+    public function filterJobs($query, $params, $search = '')
+    {
+
+        $filters = json_encode(['and' => $params]);
+
+        $url = 'https://api.catsone.com/v3/' . $query . '?query=' . $search;
+
+        $apiKey = '52190b469513a91f73c29789304acd48';
+        $content = 'Content-type: application/json';
+        $headers = array('authorization: Token ' . $apiKey, $content);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $filters);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($response);
+    }
+
 }
+
+
+
+
+
+
