29856d80a1f5b7638ed7fe7957041cacfd5a4267
Frankois21 <francois.ponceau@gmail.com>
2017-06-30 19:53:29 +0200

    design offres

---
+++ web/js/offres.js
@@ -0,0 +1,23 @@
+/**
+ * Created by francois on 30/06/2017.
+ */
+
+// jQuery(document).ready(function(voirplus){
+//
+//     voirplus(".card-offres").hover(function(event) {
+//
+//         $(this).toggle();
+//     });
+//
+// });
+
+
+/*jQuery(document).ready(function(voirPlus){
+
+    voirPlus(".card-content").hover(function(event) {
+
+        $(".offres").toggle();
+    });
+
+
+});*/
